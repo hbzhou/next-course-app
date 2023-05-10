11b479dabfedf8283d83b5c0484b439ba4eae218
Jeremy Zhou <jeremygilbert@126.com>
2023-05-10 22:24:25 +0800

    search bar with search icon

--- components/courses/SearchBar.tsx
+++ components/courses/SearchBar.tsx
@@ -13,8 +13,8 @@ const SearchBar: React.FC<Props> = ({ handleSearch }) => {
   };
   return (
     <div className='flex w-1/2'>
-      <Input ref={inputRef} placeholder='Enter Course Name' className=' border-orange-200 w-72' />
-      <Button className='btn-square w-40 mx-4' onClick={search}>
+      <Input placeholder='Enter Course Name' className='w-2/4' icon='search' />
+      <Button className='w-40 !mx-4' color='linkedin' onClick={search}>
         Search
       </Button>
     </div>
