7d2661ba89c27165129568b602fa4160239e3da4
Jeremy Zhou <jeremygilbert@126.com>
2023-05-11 22:27:54 +0800

    feact creating course with react hook form

--- pages/courses/create.tsx
+++ pages/courses/create.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import Button from "../../components/common/Button";
-import Input from "../../components/common/Input";
-import Title from "../../components/common/Title";
-import Select from "react-select";
-import { trpc } from "../../server/trpc";
 import { format } from "date-fns";
 import { useRouter } from "next/router";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Container, Form } from "semantic-ui-react";
+import Button from "../../components/common/Button";
 import Dropdown from "../../components/common/Dropdown";
+import Input from "../../components/common/Input";
 import TextArea from "../../components/common/TextArea";
-import { Container } from "semantic-ui-react";
+import Title from "../../components/common/Title";
+import { trpc } from "../../server/trpc";
 
 const CreateCourse = () => {
   const ctx = trpc.useContext();
@@ -17,11 +15,14 @@ const CreateCourse = () => {
     register,
     handleSubmit,
     control,
+    setValue,
+    trigger,
     formState: { errors },
   } = useForm<Course>();
   const router = useRouter();
   const mutation = trpc.course.createCourse.useMutation();
   const onSubmit: SubmitHandler<Course> = (data) => {
+    console.log(data);
     const creationDate = format(new Date(), "yyyy-MM-dd");
     mutation.mutateAsync(
       { ...data, creationDate },
@@ -34,38 +35,50 @@ const CreateCourse = () => {
     );
   };
 
-  const authorOptions = trpc.author.authors.useQuery().data?.map((author: Author) => {
-    return { key: author.id, text: author.name, value: author.name };
-  });
+  const authorOptions =
+    trpc.author.authors.useQuery().data?.map((author: Author) => {
+      return { text: author.name, value: author.id };
+    }) || [];
 
   return (
     <Container className='!w-2/5 p-4'>
       <Title>Create Course</Title>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='m-4'>
-          <div className='font-sans text-lg'>Title</div>
-          <Input {...register("title", { required: true })} />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Field>
+          <label>Title</label>
+          <input placeholder='Enter title' {...register("title", { required: true })} />
           <div>{errors.title && <span className='mx-1 text-red-500'>Required</span>}</div>
-        </div>
-        <div className='m-4'>
-          <div className='font-sans text-lg'>Duration</div>
-          <Input type='number' {...register("duration", { required: true, valueAsNumber: true })} />
+        </Form.Field>
+        <Form.Field>
+          <label>Duration</label>
+          <input type='number' placeholder='Enter duration' {...register("duration", { required: true, valueAsNumber: true })} />
           <div>{errors.duration && <span className='mx-1 text-red-500'>Required</span>}</div>
-        </div>
-        <div className='m-4'>
-          <div className='font-sans text-lg'>Authors</div>
-          <Dropdown placeholder='Authors' options={authorOptions} />
-          <div>{errors.authors && <span className='mx-1 text-red-500'>Required</span>}</div>
-        </div>
-        <div className='m-4'>
-          <div className='font-sans text-lg'>Description</div>
-          <TextArea rows={4} className='block p-2.5 w-full' {...register("description", { required: true })} />
+        </Form.Field>
+        <Form.Field>
+          <label>Authors</label>
+          <Form.Dropdown
+            fluid
+            multiple
+            selection
+            placeholder='Enter Authors'
+            options={authorOptions}
+            {...register("authors", { required: true })}
+            onChange={async (e, { name, value }) => {
+              setValue(name, value);
+              await trigger("authors");
+            }}
+          />
+          {errors.authors && <span className='mx-1 text-red-500'>Required</span>}
+        </Form.Field>
+        <Form.Field>
+          <label>Description</label>
+          <textarea rows={4} className='w-full' {...register("description", { required: true })} />
           <div>{errors.description && <span className='mx-1 text-red-500'>Required</span>}</div>
-        </div>
-        <div className='m-4'>
+        </Form.Field>
+        <div>
           <Button color='linkedin'>Create Course</Button>
         </div>
-      </form>
+      </Form>
     </Container>
   );
 };
