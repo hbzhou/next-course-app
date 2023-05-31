d3ff6315d15f7e326f4db73f58270198ad73fcbb
Jeremy Zhou <jeremygilbert@126.com>
2023-05-31 22:08:51 +0800

    adding next-auth for authorization

--- components/navbar/Profile.tsx
+++ components/navbar/Profile.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Button from "../common/Button";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Profile: React.FC = () => {
+  const { data: session } = useSession();
   return (
-    <div className='flex gap-6 items-center'>
-      <div className='font-bold font-mono text-2xl'>Jeremy</div>
-      <div>
-        <Button color='linkedin'>Logout</Button>
-      </div>
+    <div>
+      {session && session.user ? (
+        <div className='flex gap-6 items-center'>
+          <div className='font-bold font-mono text-2xl'>{session.user.name}</div>
+          <Button color='linkedin' onClick={() => signOut()}>
+            Logout
+          </Button>
+        </div>
+      ) : (
+        <Button color='linkedin' onClick={() => signIn()}>
+          Login
+        </Button>
+      )}
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import type { AppProps, AppType } from "next/app";
 import Navbar from "../components/navbar/Navbar";
 import { trpc } from "../server/trpc";
+import { SessionProvider } from "next-auth/react";
 
 import "../styles/global.css";
 import "semantic-ui-css/semantic.min.css";
 
 const App: AppType = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />;
-    </>
+    </SessionProvider>
   );
 };
 
