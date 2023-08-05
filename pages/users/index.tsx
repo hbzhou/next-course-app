b911d088b6872a3d6f23cf11fbf772b938cb9406
Jeremy Zhou <jeremygilbert@126.com>
2023-08-05 16:42:00 +0800

    refactor users page

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -4,37 +4,47 @@ import { useUsers } from "../../service/users.hook";
 import Link from "next/link";
 
 const Users = () => {
-  const headers = ["Name", "Email", "Password", "Role", ""];
   const { data: users, isLoading } = useUsers();
   if (isLoading) {
     return <Loader />;
   }
   return (
     <Table>
-      <Table.Header>
-        <Table.Row>
-          {headers.map((header) => {
-            return <Table.HeaderCell key={header}>{header}</Table.HeaderCell>;
-          })}
-        </Table.Row>
-      </Table.Header>
+      <Header />
       <Table.Body>
-        {users?.map((user) => {
-          const ref = `/users/${user.id}`;
-          return (
-            <Table.Row key={user.id}>
-              <Table.Cell>
-                <Link href={ref}>{user.name}</Link>
-              </Table.Cell>
-              <Table.Cell>{user.email}</Table.Cell>
-              <Table.Cell>{user.password}</Table.Cell>
-              <Table.Cell>{user.role}</Table.Cell>
-              <Table.Cell></Table.Cell>
-            </Table.Row>
-          );
-        })}
+        {users?.map((user) => (
+          <Row user={user} key={user.id}></Row>
+        ))}
       </Table.Body>
     </Table>
+  );
+};
+
+const Header = () => {
+  const headers = ["Name", "Email", "Password", "Role", ""];
+  return (
+    <Table.Header>
+      <Table.Row>
+        {headers.map((header) => {
+          return <Table.HeaderCell key={header}>{header}</Table.HeaderCell>;
+        })}
+      </Table.Row>
+    </Table.Header>
+  );
+};
+
+const Row: React.FC<{ user: User }> = ({ user }) => {
+  const ref = `/users/${user.id}`;
+  return (
+    <Table.Row key={user.id}>
+      <Table.Cell>
+        <Link href={ref}>{user.name}</Link>
+      </Table.Cell>
+      <Table.Cell>{user.email}</Table.Cell>
+      <Table.Cell>{user.password}</Table.Cell>
+      <Table.Cell>{user.role}</Table.Cell>
+      <Table.Cell></Table.Cell>
+    </Table.Row>
   );
 };
 
